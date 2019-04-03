6a7f85a620f31b5fa3a2edc4dfc71b46a760b944
Vladislav Mamonov <VMwork0123@gmail.com>
2019-04-03 10:58:14 +0700

    Add output function

--- src/functions.h
+++ src/functions.h
@@ -9,5 +9,6 @@ void input(circle* f1, circle* f2, circle* f3);
 void area_calculation(circle* f1, circle* f2, circle* f3);
 void perimeter_calculation(circle* f1, circle* f2, circle* f3);
 int intersection_definition(circle* f1, circle* f2);
+void output(circle* f1, circle* f2, circle* f3);
 
 #endif
