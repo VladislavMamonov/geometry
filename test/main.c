c4c4236f8f4b47546e49849b84aba85a2dca6f4b
Vladislav Mamonov <VMwork0123@gmail.com>
2019-04-10 18:46:16 +0700

    Use clang-format plugin for tests

--- test/main.c
+++ test/main.c
@@ -1,8 +1,8 @@
 #define CTEST_MAIN
 #define CTEST_COLOR_OK
 
-#include <functions.h>
 #include <ctest.h>
+#include <functions.h>
 
 int main(int argc, const char** argv)
 {
@@ -11,78 +11,78 @@ int main(int argc, const char** argv)
 
 CTEST(area_calculation_test, result_test)
 {
-	//Given
-	circle f;
-	f.radius = 4.5; 
+    // Given
+    circle f;
+    f.radius = 4.5;
 
-	//When
-	const double result = area_calculation(&f);
+    // When
+    const double result = area_calculation(&f);
 
-	//Then
-	const double expected = 63.617251;
-	ASSERT_EQUAL(expected, result);
+    // Then
+    const double expected = 63.617251;
+    ASSERT_EQUAL(expected, result);
 }
 
 CTEST(perimeter_calculation_test, result_test)
 {
-	//Given
-	circle f;
-	f.radius = 1.5;
+    // Given
+    circle f;
+    f.radius = 1.5;
 
-	//When
-	const double result = perimeter_calculation(&f);
+    // When
+    const double result = perimeter_calculation(&f);
 
-	//Then
-	const double expected = 9.424778;
-	ASSERT_EQUAL(expected, result);	
+    // Then
+    const double expected = 9.424778;
+    ASSERT_EQUAL(expected, result);
 }
 
 CTEST(intersection_definition_test, same_center_coordinates_test)
 {
-	//Given
-	circle f1, f2;
-	f1.x = 3.17;
-	f2.x = 3.17;
-	f2.y = -10;
-	f2.y = -10;
+    // Given
+    circle f1, f2;
+    f1.x = 3.17;
+    f2.x = 3.17;
+    f2.y = -10;
+    f2.y = -10;
 
-	//When
-	const double result = intersection_definition(&f1, &f2);
+    // When
+    const double result = intersection_definition(&f1, &f2);
 
-	//Then
-	ASSERT_FALSE(result);
+    // Then
+    ASSERT_FALSE(result);
 }
 
 CTEST(intersection_definition_test, intersection_false)
 {
-	circle f1, f2;
-	f1.x = 0;
-	f1.y = 6;
-	f1.radius = 2;
-	f2.x = 7;
-	f2.y = 6;
-	f2.radius = 5;
+    circle f1, f2;
+    f1.x = 0;
+    f1.y = 6;
+    f1.radius = 2;
+    f2.x = 7;
+    f2.y = 6;
+    f2.radius = 5;
 
-	//When
-	const double result = intersection_definition(&f1, &f2);
+    // When
+    const double result = intersection_definition(&f1, &f2);
 
-	//Then
-	ASSERT_FALSE(result);
+    // Then
+    ASSERT_FALSE(result);
 }
 
 CTEST(intersection_definition_test, intersection_true)
 {
-	circle f1, f2;
-	f1.x = 0;
-	f1.y = 6;
-	f1.radius = 2;
-	f2.x = 7;
-	f2.y = 6;
-	f2.radius = 5.1;
+    circle f1, f2;
+    f1.x = 0;
+    f1.y = 6;
+    f1.radius = 2;
+    f2.x = 7;
+    f2.y = 6;
+    f2.radius = 5.1;
 
-	//When
-	const double result = intersection_definition(&f1, &f2);
+    // When
+    const double result = intersection_definition(&f1, &f2);
 
-	//Then
-	ASSERT_TRUE(result);
+    // Then
+    ASSERT_TRUE(result);
 }
