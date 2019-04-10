96a8073c433f904d9dddeb777251ab254fab6003
Vladislav Mamonov <VMwork0123@gmail.com>
2019-04-10 18:40:50 +0700

    Add circle intersection test

--- test/main.c
+++ test/main.c
@@ -36,3 +36,53 @@ CTEST(perimeter_calculation_test, result_test)
 	const double expected = 9.424778;
 	ASSERT_EQUAL(expected, result);	
 }
+
+CTEST(intersection_definition_test, same_center_coordinates_test)
+{
+	//Given
+	circle f1, f2;
+	f1.x = 3.17;
+	f2.x = 3.17;
+	f2.y = -10;
+	f2.y = -10;
+
+	//When
+	const double result = intersection_definition(&f1, &f2);
+
+	//Then
+	ASSERT_FALSE(result);
+}
+
+CTEST(intersection_definition_test, intersection_false)
+{
+	circle f1, f2;
+	f1.x = 0;
+	f1.y = 6;
+	f1.radius = 2;
+	f2.x = 7;
+	f2.y = 6;
+	f2.radius = 5;
+
+	//When
+	const double result = intersection_definition(&f1, &f2);
+
+	//Then
+	ASSERT_FALSE(result);
+}
+
+CTEST(intersection_definition_test, intersection_true)
+{
+	circle f1, f2;
+	f1.x = 0;
+	f1.y = 6;
+	f1.radius = 2;
+	f2.x = 7;
+	f2.y = 6;
+	f2.radius = 5.1;
+
+	//When
+	const double result = intersection_definition(&f1, &f2);
+
+	//Then
+	ASSERT_TRUE(result);
+}
